ed0f892e4df030b442882541b8aaf78efd9d827f
Barış karakuş <bariscan9788@gmail.com>
2024-08-15 08:40:55 +0300

    ....

--- interval/routes/routes.go
+++ interval/routes/routes.go
@@ -1,23 +1,24 @@
 package routes
 
 import (
-    "github.com/gin-gonic/gin"
-    "bookservice/interval/controller"
+	"bookservice/interval/controller"
+
+	"github.com/gin-gonic/gin"
 )
 
 func SetupRouter() *gin.Engine {
-	
+
 	gin.SetMode(gin.ReleaseMode)
 
-    r := gin.Default()
-	
+	r := gin.Default()
+
 	bookController := controller.NewUserController()
-    
+
 	r.POST("/books", bookController.CreateBook)
-    r.GET("/books", bookController.GetAllBooks)
-    r.GET("/books/:id", bookController.GetBookById)
+	r.GET("/books", bookController.GetAllBooks)
+	r.GET("/books/:id", bookController.GetBookById)
 	r.DELETE("/books/:id", bookController.DeleteBookById)
 	r.PATCH("/books/:id", bookController.UpdatePriceById)
-    
+
 	return r
 }
